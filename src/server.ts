c6a7b2e7cb9c19e13a896a8723fe789999d8e81e
Ed Johnson <ed@neon.fish>
2021-02-25 12:05:23 +0000

    Add convenience methods for the server to patch existing states

--- src/server.ts
+++ src/server.ts
@@ -69,6 +69,13 @@ export class ServerUiState<T extends BaseUiStateType> {
 
   }
 
+  /**
+   * Read a snapshot of the current state value of the given key
+   */
+  get<TKey extends keyof T>(key: TKey) {
+    return this._state[key].value as T[TKey]["value"];
+  }
+
   update<TKey extends keyof T>(key: TKey, value: T[TKey]["value"]) {
     this._state[key].next(value);
 
@@ -85,10 +92,43 @@ export class ServerUiState<T extends BaseUiStateType> {
   }
 
   /**
-   * Read a snapshot of the current state value of the given key
+   * Apply a partial update of the value with the specified key using Object.assign().
+   * @param key The key of the specified state to update
+   * @param update The partial update object with which to patch the existing state
    */
-  get<TKey extends keyof T>(key: TKey) {
-    return this._state[key].value as T[TKey]["value"];
+  updateObjectPartial<TKey extends keyof T>(key: TKey, update: Partial<T[TKey]["value"]>) {
+
+    const value = this.get(key);
+    if ((value as object) instanceof Array) {
+      throw new Error(`Cannot apply a partial object update to state with key "${key}", as the state value is an array`);
+    }
+
+    Object.assign(value, update);
+    this.update(key, value);
+  }
+
+  /**
+   * After finding the value for the specified key, the desired element is found using the specified function.
+   * Ths element is then updated to be the given value, of if no matching element was found the new value is appended to the list.
+   * @param key The key of th target state value
+   * @param findFn Function to find the desired element to update
+   * @param newValue The new value to which to set the element
+   */
+  updateArrayElement<TKey extends keyof T, TElem extends T[TKey]["value"]>(key: TKey, findFn: (item: TElem, index: number) => boolean, newValue: TElem) {
+
+    const keyValue = this.get(key);
+    if (!((keyValue as object) instanceof Array)) {
+      throw new Error(`Cannot update element in list for state key "${key}", as the state value is not a list`);
+    }
+    const stateList = keyValue as Array<T[TKey]["value"][number]>;
+
+    const targetState = stateList.find(findFn);
+    if (targetState) {
+      Object.assign(targetState, newValue);
+    } else {
+      stateList.push(newValue);
+    }
+    this.update(key, stateList);
   }
 
 }
